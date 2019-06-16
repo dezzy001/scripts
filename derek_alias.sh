ea688ced13ed803add2f4d2ecd3008df14394360
dezzy001 <dezzy001@gmail.com>
2019-06-16 16:22:43 +1000

    first commit

---
+++ derek_alias.sh
@@ -0,0 +1,16 @@
+# Vi alias
+alias vi="vi -p"
+alias sali=". ~/scripts/derek_alias.sh;clear"
+alias ali="vi ~/scripts/derek_alias.sh"
+alias vimrc="vi ~/.vimrc"
+alias svimrc="source ~/.vimrc"
+
+
+
+alias l="ls -la"
+alias cl="clear"
+
+# derek quick cd alias
+alias de="cd $dd"
+alias decode="cd $dd/derek_code"
+
---
+++ derek_loadout.sh
@@ -0,0 +1,12 @@
+# startup script
+
+# dezzy desktop = dd
+dd="/mnt/c/Users/Dezzy/Desktop"
+export dd 
+echo " - environment variables setup "
+cd $dd/derek_code
+
+. ~/scripts/derek_alias.sh
+echo " - alias setup "
+
+echo " - loadout complete "
